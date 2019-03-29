d23a1fa0cb1c01b4d89426ee58329b08fe19aed9
Robin <robinlanrui@pku.edu.cn>
2019-03-29 13:48:33 +0800

    mnist code and some simple code

---
+++ simple code/tf_04.py
@@ -0,0 +1,14 @@
+#coding: utf - 8
+import tensorflow as tf
+#define input and parameter
+x = tf.placeholder(tf.float32, shape=(None, 2))
+w1 = tf.Variable(tf.random_normal([2,3], stddev=1,seed=1)) #normal distribution
+w2 = tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
+# define forward
+a = tf.matmul(x, w1)
+y = tf.matmul(a, w2)
+# session compute
+with tf.Session() as sess:
+    init_op = tf.global_variables_initializer()
+    sess.run(init_op)
+    print("y in tf_03,py is: \n",sess.run(y, feed_dict={x:[[0.7, 0.5], [0.2, 0.3], [0.3, 0.4], [0.4, 0.5]]}))
